9cb8504eac9aff1e522572efeecb4eaeacb97599
Harvey Liam <83070312+liamVScode@users.noreply.github.com>
2023-04-24 18:11:34 +0700

    first commit

---
+++ src/test/java/com/javatachiedemo/SpringAzureDemo5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatachiedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAzureDemo5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
